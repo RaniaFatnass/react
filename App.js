5674f6c9104c08bb1c87b7cdbe940331c5fa6dcf
RaniaFatnass <50364146+RaniaFatnass@users.noreply.github.com>
2020-08-05 15:44:20 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+      <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div  class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+      </div>
+      
+      <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+      <label class="form-check-label" for="exampleCheck1"></label>
+      <button type="submit" class="btn btn-primary">submit</button>
+      </div>
+        
+      
+      </form>
+    </div>
+  );
+}
+
+export default App;
